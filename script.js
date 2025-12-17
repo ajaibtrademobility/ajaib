9650e0a4d3d1f5f9289beba3bff0aadd56ea4c7f
ajaibtrademobility <ajaibtrademobility@gmail.com>
2025-12-18 04:11:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("scroll", () => {
+document.querySelectorAll(".glass").forEach(card => {
+const rect = card.getBoundingClientRect();
+if (rect.top < window.innerHeight) {
+card.style.boxShadow = "0 0 30px rgba(0,245,212,0.2)";
+}
+});
+});
 
